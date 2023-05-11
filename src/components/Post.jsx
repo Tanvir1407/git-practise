5db8c634abce3fd34f2fd1c0c823e77f4524a43c
MD. Tanvir <mt14050@gmail.com>
2023-05-11 15:13:05 +0600

    c

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
 
 const Post = ({ post }) => {
-  return <div>{post.author}</div>;
+    return <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam minima quos porro dolorum impedit vero hic provident ducimus alias quasi eveniet repudiandae aperiam debitis quae, consequuntur unde iure id quia.
+    </div>;
 };
 
 export default Post;
