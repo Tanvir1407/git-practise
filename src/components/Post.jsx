de340eab3fe235c7b8063ea781bf1f31216a3cbf
MD. Tanvir <mt14050@gmail.com>
2023-05-11 15:20:05 +0600

    cng

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@
 
 const Post = ({ post }) => {
     return <div>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam minima quos porro dolorum impedit vero hic provident ducimus alias quasi eveniet repudiandae aperiam debitis quae, consequuntur unde iure id quia.
+        {post.author}
     </div>;
 };
 
