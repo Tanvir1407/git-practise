be37db7baf78b2624e74516b4db21596395b9516
MD. Tanvir <mt14050@gmail.com>
2023-05-11 14:49:06 +0600

    change post design

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/prop-types */
 
 const Post = ({ post }) => {
-  return <div>{post.author}</div>;
+    return <>
+            <div>   
+                <h1>Name of this Card {post.author}</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt.</p>
+            </div>
+          </>;
 };
 
 export default Post;
