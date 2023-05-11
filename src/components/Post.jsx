5bdcccbe3fb411eb4d464cce2867a7b9a0fb8797
MD. Tanvir <mt14050@gmail.com>
2023-05-11 15:18:47 +0600

    git add new info

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable react/prop-types */
 
 const Post = ({ post }) => {
-  return <div>{post.author}</div>;
+    return <div>
+        {post.author}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, libero.</p>
+    </div>;
+    
 };
 
 export default Post;
